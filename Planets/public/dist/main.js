3cf3f8ed50de36b55e260d67e84360940a626b80
William Fernandes <wiilfern1910@outlook.com>
2024-12-10 19:55:51 -0300

    Listando Planets

--- Planets/public/dist/main.js
+++ Planets/public/dist/main.js
@@ -1,8 +1,36 @@
 // Array de Planetas do meu SYSTEM
-var planets = [];
+let planets = [];
+// Função pra Pickar um Planeta pelo nome
+function getPlanet(name) {
+    return planets.find((planet) => planet.name == name);
+}
 // Função que gera um Planeta e adiciona ele no meu Array
 function buildPlanet(name, coordinates, state) {
-    var planet = { name: name, coordinates: coordinates, state: state, satellites: [] };
+    let planet = { name, coordinates, state, satellites: [] };
     planets.push(planet);
-    alert("O Planeta ".concat(name, " foi adicionado em seu Sistema Solar!"));
+    alert(`O Planeta ${name} foi adicionado em seu Sistema Solar!`);
+}
+// Função pra atualizar o State de determinado Planeta
+function updatePlanetState(planet, state) {
+    planet.state = state;
+    alert(`O estado do Planeta ${planet.name} foi alterado para ${state.toUpperCase()}`);
+}
+// Adicionando um Satélite a um Planeta
+function addSatellite(planet, name) {
+    planet.satellites.push(name);
+    alert(`O satélite ${name} foi adicionado na Órbita do Planeta ${planet.name}`);
+}
+// Função que remove Satélite do Planeta
+function popSatellite(planet, name) {
+    planet.satellites.splice(planet.satellites.indexOf(name), 1);
+    alert(`O Satélite ${name.toUpperCase()} foi removido da Órbita de ${planet.name}!`);
+}
+// Função que Lista todos os planetas e suas Informações
+function listPlanets() {
+    const planetsList = planets.reduce((s, planet) => s +
+        `Planeta: ${planet.name}\n
+       Coordenadas: ${JSON.stringify(planet.coordinates)}\n
+       Situação: ${planet.state}\n
+        Satélites: ${JSON.stringify(planet.satellites)}\n\n`, "");
+    alert(planetsList);
 }
--- Planets/src/main.ts
+++ Planets/src/main.ts
@@ -11,9 +11,54 @@ type Planet = {
 // Array de Planetas do meu SYSTEM
 let planets: Planet[] = [];
 
+// Função pra Pickar um Planeta pelo nome
+function getPlanet(name: string) {
+  return planets.find((planet) => planet.name == name);
+}
+
 // Função que gera um Planeta e adiciona ele no meu Array
 function buildPlanet(name: string, coordinates: coordinates, state: state) {
   let planet: Planet = { name, coordinates, state, satellites: [] };
   planets.push(planet);
   alert(`O Planeta ${name} foi adicionado em seu Sistema Solar!`);
+}
+
+// Função pra atualizar o State de determinado Planeta
+function updatePlanetState(planet: Planet, state: state) {
+  planet.state = state;
+  alert(
+    `O estado do Planeta ${
+      planet.name
+    } foi alterado para ${state.toUpperCase()}`
+  );
+}
+
+// Adicionando um Satélite a um Planeta
+function addSatellite(planet: Planet, name: string) {
+  planet.satellites.push(name);
+  alert(
+    `O satélite ${name} foi adicionado na Órbita do Planeta ${planet.name}`
+  );
+}
+
+// Função que remove Satélite do Planeta
+function popSatellite(planet: Planet, name: string) {
+  planet.satellites.splice(planet.satellites.indexOf(name), 1);
+  alert(
+    `O Satélite ${name.toUpperCase()} foi removido da Órbita de ${planet.name}!`
+  );
+}
+
+// Função que Lista todos os planetas e suas Informações
+function listPlanets() {
+  const planetsList: string = planets.reduce(
+    (s, planet) =>
+      s +
+      `Planeta: ${planet.name}\n
+       Coordenadas: ${JSON.stringify(planet.coordinates)}\n
+       Situação: ${planet.state}\n
+        Satélites: ${JSON.stringify(planet.satellites)}\n\n`,
+    ""
+  );
+  alert(planetsList);
 }
