fafe5ea1ca9ce81b50955ced1cd1f41c080e8483
William Fernandes <wiilfern1910@outlook.com>
2025-04-03 10:26:27 -0300

    MVC

---
+++ MVC/src/index.js
@@ -0,0 +1,12 @@
+const express =require("express")
+const path = require("node:path")
+const router = require("./routes")
+const app = express()
+
+app.set("view engine","ejs")
+app.set("views",path.join(__dirname,"views"))
+app.use(express.static("public"))
+app.use(express.urlencoded({extended:true}))
+app.use(router)
+
+app.listen(3000)
